122a755c9b6f73b3a08ac24e1012ed9931398de7
Abigail Chin <chinabigail08@gmail.com>
2016-08-05 14:49:12 -0700

    will recognize when post created and will print post

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -12,8 +12,8 @@ import Parse
 class TimelineViewController: UIViewController {
     
     var photoTakingHelper: PhotoTakingHelper?
-    
-
+    var posts: [Post] = []
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +22,37 @@ class TimelineViewController: UIViewController {
         self.tabBarController?.delegate = self
     }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // fetches the query of follow relationships
+        let followingQuery = PFQuery(className: "Follow")
+        followingQuery.whereKey("fromUser", equalTo:PFUser.currentUser()!)
+        
+        // adding pictures from those whom u are following
+        let postsFromFollowedUsers = Post.query()
+        postsFromFollowedUsers!.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+        
+        // retrieve posts the current user has posted
+        let postsFromThisUser = Post.query()
+        postsFromThisUser!.whereKey("user", equalTo: PFUser.currentUser()!)
+        
+        // will return contraints from above two queries
+        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
+        // resolve pointer and download all info about user
+        query.includeKey("user")
+        // formats timeline to show most recent pictures
+        query.orderByDescending("createdAt")
+        
+        // kick off network request
+        query.findObjectsInBackgroundWithBlock {(result: [PFObject]?, error: NSError?) -> Void in
+            // receive all posts that meet requirements and try to store in array, if empty, store empty array
+            self.posts = result as? [Post] ?? []
+            // refresh table view once receiving all new posts
+            self.tableView.reloadData()
+        }
+    }
+
 }
 
 
@@ -45,6 +76,19 @@ extension TimelineViewController: UITabBarControllerDelegate {
         } else {
             return true
         }
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // 2
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell")!
+        
+        cell.textLabel!.text = "Post"
+
+        return cell
     }
 }
 
